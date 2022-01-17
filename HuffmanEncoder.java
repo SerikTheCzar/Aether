fc3455a682fb4e8fb6f551a7c870ff899d7fecbe
Serik Czarnecki <serikaczarnecki@gmail.com>
2022-01-17 12:20:16 -0500

    made the code for huffman encoder a class that works
    huffman encoder is now an object type called HashMap

--- HuffmanEncoder.java
+++ HuffmanEncoder.java
@@ -2,7 +2,7 @@
 import java.util.PriorityQueue;
 import java.util.HashMap;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 class Node {
     Node left;
     Node right;
@@ -11,52 +11,35 @@ class Node {
 
 public class HuffmanEncoder {
     
-    public Node encodeData(int[] data) {
+    public HuffmanEncoder(int[] data) {
 
     }
 
-    public static void main(String args[]) {
+    public HashMap frequency(int[] zigzag) {
         PriorityQueue<Node> dataQueue = new PriorityQueue<Node>();
 
-        HashMap<Double, Integer> quantizeFreq = new HashMap<Double, Integer>();
+        int[]matrix = zigzag;
+        HashMap<Integer, Integer> quantizeFreq = new HashMap<Integer, Integer>();
 
-        double[][] quantizeTable = {
-			{16, 11, 10, 16, 24, 40, 51, 61},
-			{12, 12, 14, 19, 26, 58, 60, 55},
-			{14, 13, 16, 24, 40, 57, 69, 56},
-			{14, 17, 22, 29, 51, 87, 80, 62},
-			{18, 22, 37, 56, 68, 109, 103, 77},
-			{24, 35, 55, 64, 81, 104, 113, 92},
-			{49, 64, 78, 87, 103, 121, 120, 101},
-			{72, 92, 95, 98, 112, 100, 103, 99}
-		};
 
-        ArrayList<Integer> zigzag = new ArrayList<Integer>(
-            Arrays.asList(11, 12, 14, 12, 10, 16, 14, 13, 14, 18, 17, 16, 
-                        19, 24, 40, 26, 24, 22, 22, 24, 49, 35, 37, 29, 40, 58, 51, 61, 
-                        60, 57, 51, 56, 55, 64, 72, 92, 78, 64, 68, 87, 69, 55, 56, 80, 
-                        109, 81, 87, 95, 98, 103, 104, 103, 62, 77, 113, 121, 112, 100, 
-                        120, 92, 101, 103, 99)); 
 
-        for (Integer i : zigzag) {
-            if (quantizeFreq.containsKey(i)) {
-                quantizeFreq.put(i, quantizeFreq.get(i)++);
+        for(int key : matrix) {
+            if(quantizeFreq.containsKey(key)) {
+                int occurrence = quantizeFreq.get(key);
+                occurrence++;
+                quantizeFreq.put(key, occurrence);
             } else {
-                quantizeFreq.put(i, 1);
+                quantizeFreq.put(key, 1);
             }
         }
-
-
-        for (Integer i : quantizeFreq.keySet()) {
-            System.out.println(i);
+   
+        for(Integer key : quantizeFreq.keySet()) {
+            int occurrence = quantizeFreq.get(key);
+            System.out.println(key + " occur " + occurrence + " time(s).");
         }
-
-
-        int length = 8;
-
         // char[] charArray = { 'A', 'B', 'C', 'D' };
         // int[] charfreq = { 5, 1, 6, 3 };
-
+     return quantizeFreq;
     }
 }
 
--- compress.java
+++ compress.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.util.PriorityQueue;
+import java.util.HashMap;
 import javax.swing.ImageIcon;
 import java.awt.*;
 import java.io.*;
@@ -73,6 +75,13 @@ public class compress extends JFrame
 		double[][]qcrt = qt2.quantizeC(dct2table);
 		zigzag zz = new zigzag(qlt);
 		int[]zag = zz.array(qlt);
+		int[] ziggity = new int[]{11, 12, 14, 12, 10, 16, 14, 13, 14, 18, 17, 16, 
+                        19, 24, 40, 26, 24, 22, 22, 24, 49, 35, 37, 29, 40, 58, 51, 61, 
+                        60, 57, 51, 56, 55, 64, 72, 92, 78, 64, 68, 87, 69, 55, 56, 80, 
+                        109, 81, 87, 95, 98, 103, 104, 103, 62, 77, 113, 121, 112, 100, 
+                        120, 92, 101, 103, 99};
+        HuffmanEncoder test = new HuffmanEncoder(ziggity);
+        HashMap<Integer, Integer> output = test.frequency(ziggity);
 		//for loop for testing output
 		/*for(int v=0;v<ym.length;v++){
 			for(int q=0;q<ym[v].length;q++){
